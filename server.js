42adf0c235c0914ff3ef76c91102ef4c861fb201
Kevin He <kevin.he@salesforce.com>
2024-04-11 15:14:43 -0700

    minimal poc

---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import { createServer as createViteServer } from "vite";
+
+const shell = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>My Minimal Website</title>
+</head>
+<body>
+<div id="root"><!--ssr-outlet--></div>
+<script type="module" src="/entry-client.jsx"></script>
+</body>
+</html>
+`;
+
+async function createServer() {
+  const app = express();
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+  });
+
+  // Use vite's connect instance as middleware. If you use your own
+  // express router (express.Router()), you should use router.use
+  // When the server restarts (for example after the user modifies
+  // vite.config.js), `vite.middlewares` is still going to be the same
+  // reference (with a new internal stack of Vite and plugin-injected
+  // middlewares). The following is valid even after restarts.
+  app.use(vite.middlewares);
+
+  app.use("*", async (req, res, next) => {
+    const url = req.originalUrl;
+
+    try {
+      // 1. Apply Vite HTML transforms. This injects the Vite HMR client,
+      //    and also applies HTML transforms from Vite plugins, e.g. global
+      //    preambles from @vitejs/plugin-react
+      const template = await vite.transformIndexHtml(url, shell);
+
+      // 2. Load the server entry. ssrLoadModule automatically transforms
+      //    ESM source code to be usable in Node.js! There is no bundling
+      //    required, and provides efficient invalidation similar to HMR.
+      const { render } = await vite.ssrLoadModule("/entry-server.jsx");
+
+      // 3. render the app HTML. This assumes entry-server.js's exported
+      //     `render` function calls appropriate framework SSR APIs,
+      //    e.g. ReactDOMServer.renderToString()
+      const appHtml = await render(url);
+
+      // 4. Inject the app-rendered HTML into the template.
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+
+      // 5. Send the rendered HTML back.
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    } catch (e) {
+      // If an error is caught, let Vite fix the stack trace so it maps back
+      // to your actual source code.
+      vite.ssrFixStacktrace(e);
+      next(e);
+    }
+  });
+
+  app.listen(5173);
+  console.log("Server running at http://localhost:5173");
+}
+
+createServer();
